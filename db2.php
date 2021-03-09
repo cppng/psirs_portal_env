45cea08ecf1e3ea48fefd0746c5973ff35ce398b
= <larypetero@gmail.com>
2021-03-09 20:34:20 +0100

    changed db 2 to mysql

--- db2.php
+++ db2.php
@@ -8,13 +8,120 @@ class Db2 extends Db{
 
     public function __construct(){
 
-        $this->host = "localhost";
-        $this->database = "psirs4";
-        $this->username = "postgres";
-        $this->password = "test123";
+        $this->host = "35.170.84.126";
+        $this->database = "psirs_portal";
+        $this->username = "root2";
+        $this->password = "duffHat@Zrc8";
         $this->log_tag = "Db2";
     }
 
+    public function insert_fresh_rows($table, $rows){ 
+        
+
+            foreach($rows as $row){
+                unset($row['id']);
+                unset($row['supplier']);
+                unset($row['is_company']);
+                unset($row['customer']);
+                unset($row['employee']);
+                $query = $this->array_to_query($row, "users");
+             
+               
+                mysqli_query($this->get_connection(), $query) or $this->log(mysqli_error($this->get_connection()));
+
+            }
+ 
+
+        $this->save_last_update_time(); 
+ 
+    }
+
+
+    private function array_to_query($insData, $table){
+
+        foreach($insData as $key => $value){
+
+            if(empty($insData[$key])){
+                unset($insData[$key]);
+            }
+            else if(is_string($insData[$key])){
+
+                $insData[$key] = "\"". $insData[$key]. "\"";
+            }
+
+            
+        }
+
+        $query= "INSERT INTO $table ( " . implode(', ',array_keys($insData)) . ") VALUES (" . implode(', ',array_values($insData)) . ");";
+
+        return $query;
+    }
+
+
+    public function save_last_update_time(){ 
+
+        $date = date("d-m-Y H:i:s.u");
+        $query  = "UPDATE pacifier set last_update='$date'";
+
+        $connection = $this->get_connection(); 
+
+        $result =  mysqli_query($connection, $query) or $this->log(mysqli_error($connection)); 
+
+    }
+
+
+    public function get_entries_since($table_name, $date_column, $date){
+
+        $query = "SELECT * FROM $table_name WHERE  $date_column > '$date' ORDER BY $date_column ";
+ 
+        $result =  mysqli_query($this->get_connection(), $query) or die(mysqli_error($this->get_connection()));
+        
+        $rows = [];
+
+        while ($row = mysqli_fetch_array($result)) {
+            
+            if($row!=null){
+                 $rows[] = $row;
+            }
+          }
+  
+
+        return $rows;
+
+    }
+
+    public function get_last_sync_date(){
+
+        $query = "SELECT last_update FROM pacifier";
+        $result = mysqli_query($this->get_connection(), $query);
+
+        $rows = [];
+
+        while ($row = mysqli_fetch_row($result)) {
+            
+            if($row!=null){
+                 $rows[] = $row;
+            }
+          }
+ 
+
+        return $rows[0][0];
+
+
+    }
+
+
+    public function get_connection(){
+
+        if($this->connection == null){ 
+
+            $connection = mysqli_connect($this->host, $this->username, $this->password, $this->database) or die(mysqli_error($connection));
+
+            $this->connection = $connection;
+        }
+
+        return $this->connection;
+    } 
   
  
 }
--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require("db1.php");
 require("db2.php");
 
 $tin_table_in_db1 = "res_partner";
-$tin_table_in_db2 = "res_partner";
+$tin_table_in_db2 = "users";
 
 
 $db1 = new Db1();
@@ -20,7 +20,8 @@ $last_date_tin_db2 = $db2->get_last_sync_date();
  
    
 $fresh_rows_in_db_1 = $db1->get_entries_since($tin_table_in_db1, "create_date", $last_date_tin_db2 );
-$fresh_rows_in_db_2 = $db2->get_entries_since($tin_table_in_db1, "create_date", $last_date_tin_db1 );
+ 
+$fresh_rows_in_db_2 = $db2->get_entries_since($tin_table_in_db2, "create_date", $last_date_tin_db1 );
 
  
 
@@ -28,7 +29,9 @@ $fresh_rows_in_db_2 = $db2->get_entries_since($tin_table_in_db1, "create_date", 
  
 if(count($fresh_rows_in_db_1) > 0){
 
-        $db2->insert_fresh_rows("res_partner", $fresh_rows_in_db_1);
+        $count = count($fresh_rows_in_db_1);
+        $db2->log("Found $count fresh rows for db2");
+        $db2->insert_fresh_rows($tin_table_in_db2, $fresh_rows_in_db_1);
 }
 else{
 
@@ -37,6 +40,8 @@ else{
 
 if(count($fresh_rows_in_db_2) > 0){
 
+        $count = count($fresh_rows_in_db_2);
+        $db1->log("Found $count fresh rows for db1");
         $db1->insert_fresh_rows("res_partner", $fresh_rows_in_db_2);
 }
 else{
